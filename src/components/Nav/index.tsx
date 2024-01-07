98f893b250b07183fccde517b289a5e1a9d45e0b
김영은 <jessicak0828@gmail.com>
2024-01-07 20:13:39 +0900

    feat: create Nav
    - map linksArr for Nav's list items
    - pathname equals with link,
    - when link equals 'home', use '' instead

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,18 +1,25 @@
 import { PropsWithChildren } from 'react';
 import Link from 'next/link';
+import { PATH_NAME } from '@/constants';
+
 interface Props extends PropsWithChildren {
 	list?: string[];
 }
 
-export default function Nav({ list = ['home', 'about', 'posts', 'contact'] }: Props) {
+export default function Nav({}: Props) {
+	const linksArr = [PATH_NAME.home, PATH_NAME.about, PATH_NAME.projects, PATH_NAME.contact];
+
 	return (
 		<nav>
 			<ul className="flex-row--evenly">
-				{list.map((item: string) => (
-					<Link href={`/${item}`} key={item}>
-						<li className="p-[1rem] text-[1.6vw] font-bold">{item}</li>
-					</Link>
-				))}
+				{linksArr.map((link) => {
+					const pathname = link === PATH_NAME.home ? '' : link;
+					return (
+						<Link href={`/${pathname}`} key={link}>
+							<li className="p-4 sm:text-[1.6vw] font-bold">{link}</li>
+						</Link>
+					);
+				})}
 			</ul>
 		</nav>
 	);
