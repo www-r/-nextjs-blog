2739401239eb9897663af372780b93cbe2af2b51
김영은 <jessicak0828@gmail.com>
2024-01-07 20:20:15 +0900

    feat: create Component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
-import { PropsWithChildren } from 'react';
-import Nav from '@components/Nav';
+// import { PropsWithChildren } from 'react';
 import Link from 'next/link';
-interface Props extends PropsWithChildren {}
+import Nav from '@components/Nav';
+import { MY_NAME } from '@/constants';
+// interface Props extends PropsWithChildren {}
 
-export default function Header({ children }: Props) {
+export default function Header() {
 	return (
-		<header className="inner flex justify-between h-[10rem] w-full fixed top-0 z-50">
+		<header className="inner flex justify-between items-center h-[7.8vw] max-h-[8rem] fixed top-0 z-50 bg-[red] w-full">
 			<Link href="/">
-				<h1 className="text-[3vw] font-extrabold">{children}</h1>
+				<h1 className="text-[1.6rem] sm:text-[2.7vw] font-extrabold">{MY_NAME}</h1>
 			</Link>
 			<Nav />
 		</header>
