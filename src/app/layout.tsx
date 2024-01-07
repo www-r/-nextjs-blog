06f3359465c06a69a799b2e6dfcafea34622557b
김영은 <jessicak0828@gmail.com>
2024-01-07 20:19:03 +0900

    feat: create RootLayout
    set <body>'s max width to 4000px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,15 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 
 export const metadata: Metadata = {
-	title: "Kim Young En's Blog",
-	description: '프론트엔드 개발자 김영은의 블로그입니다.',
+	title: "Kim Young En",
+	description: '프론트엔드 개발자 김영은입니다.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ko">
-			<body>
-				<Header>Header</Header>
+			<body className="max-w-[400rem] flex-cols items-center mx-auto overflow-x-hidden">
+				<Header />
 				<main>{children}</main>
 				<Footer>Footer</Footer>
 			</body>
