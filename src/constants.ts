bb0d0372bd71eb3400cf32a3f5082bf92ce5de47
김영은 <jessicak0828@gmail.com>
2024-01-07 20:17:56 +0900

    feat: add images & constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,24 @@ export const FE = 'Front-End';
 export const BE = 'Back-End';
 export const FULL_STACK = 'Full-Stack';
 export const ALL = 'All';
+
+export const MY_NAME = 'Kim Young En';
+//About
+// const ABOUT_CATEGORY = ['Profile','Skills','Education']
+export const PROFILE_TEXT = {
+	main: [`Hi, I'm YoungEn`, 'Front-End Developer'],
+	about: [],
+};
+export const PATH_NAME = {
+	home: 'home',
+	about :'about',
+	projects:'projects',
+	contact:'contact'
+}
+
+
+export const BUTTON = {
+	contact: 'Contact me',
+	resume: 'check Resume',
+	letter: 'check Cover Letter',
+};
