abc9117c4d88ad7c70fbc0656067d66333689f1f
김영은 <jessicak0828@gmail.com>
2024-01-08 23:52:30 +0900

    design: Layout UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,17 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 
 export const metadata: Metadata = {
-	title: "Kim Young En",
+	title: 'Kim Young En',
 	description: '프론트엔드 개발자 김영은입니다.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ko">
-			<body className="max-w-[400rem] flex-cols items-center mx-auto overflow-x-hidden">
+			<body className="max-w-[160rem] h-[100vh] flex-cols items-center mx-auto overflow-x-hidden overflow-y-auto">
 				<Header />
-				<main>{children}</main>
-				<Footer>Footer</Footer>
+				<main className='h-full'>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	);
