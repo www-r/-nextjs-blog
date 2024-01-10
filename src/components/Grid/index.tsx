1c27778ac6bf7795692f6ae3bf65d210dcbd6c2b
김영은 <jessicak0828@gmail.com>
2024-01-10 13:31:37 +0900

    feat: create Component Grid
    render projectsArr with ProjectCard in Grid

---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,27 @@
+import { Project } from '@/types';
+import ProjectCard from '../ProjectCard';
+
+interface Props {
+	projects: Project[];
+}
+
+export default function Grid({ projects }: Props) {
+	try {
+		return (
+			<ul className="mx-[5vw] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[4rem] gap-y-20 p-20 bg-white min-h-[30rem]">
+				{projects.map((project) => (
+					<li key={project.id}>
+						<ProjectCard project={project} />
+					</li>
+				))}
+				{projects.map((project) => (
+					<li key={project.id}>
+						<ProjectCard project={project} />
+					</li>
+				))}
+			</ul>
+		);
+	} catch (error) {
+		console.error(error);
+	}
+}
