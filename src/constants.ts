2a377020a4bd4a41f54e448b92b986e5c862a9de
김영은 <jessicak0828@gmail.com>
2024-01-10 13:39:54 +0900

    feat: create constants

--- src/constants.ts
+++ src/constants.ts
@@ -11,15 +11,19 @@ export const MY_NAME = 'Kim Young En';
 // const ABOUT_CATEGORY = ['Profile','Skills','Education']
 export const PROFILE_TEXT = {
 	main: [`Hi, I'm YoungEn`, 'Front-End Developer'],
-	about: [],
+	about: [
+		`'우리'가 중요한 개발자, 김영은입니다. 협업, 소통, 성장을 중요하게 생각합니다.`,
+		'원활한 협업을 위해 명시적인 코드 작성 및 안정성을 높이려고 항상 노력합니다.',
+		'공부한 내용을 저만의 언어로 재구성한 기술 블로그를 운영하고 있습니다.',
+		'상황에 따라 적절하게 대처할 수 있는 유연한 개발자가 되고 싶습니다.',
+	],
 };
 export const PATH_NAME = {
 	home: 'home',
-	about :'about',
-	projects:'projects',
-	contact:'contact'
-}
-
+	about: 'about',
+	projects: 'projects',
+	contact: 'contact',
+};
 
 export const BUTTON = {
 	contact: 'Contact me',
