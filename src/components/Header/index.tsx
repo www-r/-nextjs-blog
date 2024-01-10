f953ba4f413b8846b74726c4778111be3c131cd2
김영은 <jessicak0828@gmail.com>
2024-01-10 13:34:09 +0900

    design: Header UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,12 @@
-// import { PropsWithChildren } from 'react';
 import Link from 'next/link';
 import Nav from '@components/Nav';
 import { MY_NAME } from '@/constants';
-// interface Props extends PropsWithChildren {}
 
 export default function Header() {
 	return (
-		<header className="inner flex justify-between items-center h-[7.8vw] max-h-[8rem] fixed top-0 z-50 bg-[red] w-full">
+		<header className=" border-solid border-b-[0.1rem] inner flex-row--between items-center h-[7.8vw] max-h-[8rem] max-w-[160rem] fixed top-0 z-50 w-full bg-white">
 			<Link href="/">
-				<h1 className="text-[1.6rem] sm:text-[2.7vw] font-extrabold">{MY_NAME}</h1>
+				<h1 className="text-[1.6rem] sm:text-[2.7vw]  2xl:text-[4.36rem] font-extrabold">{MY_NAME}</h1>
 			</Link>
 			<Nav />
 		</header>
