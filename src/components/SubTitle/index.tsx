0e211962c3cf8eb09a22dd4806cd97ca6159a804
김영은 <jessicak0828@gmail.com>
2024-01-10 13:25:51 +0900

    feat: create Component SubTitle

---
+++ src/components/SubTitle/index.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from 'react';
+
+interface Props extends PropsWithChildren {}
+export default function SubTitle({ children }: Props) {
+	return (
+		<h2 className="inner text-[2.5rem] font-extrabold pt-4 pb-4 border-solid border-b-[0.1rem] w-[90%] text-start">
+			{children}
+		</h2>
+	);
+}
