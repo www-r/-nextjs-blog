9c79b908fb30402564cfd6ca9c805ac86ef2d072
김영은 <jessicak0828@gmail.com>
2024-01-10 13:32:41 +0900

    feat: create Component Project Card
    render project's data in component

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,21 +1,28 @@
+import { Project } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
-import { PropsWithChildren } from 'react';
 
+interface Props {
+	project: Project;
+}
 
-export default function ProjectCard({ post}: Props) {
+export default function ProjectCard({ project }: Props) {
 	return (
-		<Link href={`/posts/${post.path}`}>
-			<div className="grid grid-rows-[3fr_2fr]">
-				<div>
-					<Image src={''} alt={post?.title} style={{ objectFit: 'contain' }} />
+		//
+		<Link href={`/projects/${project.id}`}>
+			<article className="border-solid border-[0.1rem] bg-white min-w-[20rem] max-w-[30rem] mx-auto rounded-md overflow-hidden shadow-md hover:shadow-xl hover:translate-x-1 hover:-translate-y-1">
+				<div className="center h-[20rem]">
+					<Image width={300} height={300} src={project.image} alt={project.title} style={{ objectFit: 'cover' }} />
 				</div>
-				<div>
-					<p>{post?.date}</p>
-					<h4>{post?.title}</h4>
-					<p className="card-body">{post?.description}</p>
+				<div className="h-[15rem] flex-cols justify-evenly">
+					<h4 className="border-solid border-y-[0.1rem] px-3 text-[2.5rem] font-bold">{project.title}</h4>
+					<div className="px-3 flex-row--between flex-wrap text-2xl">
+						<p>{project.description.people}</p>
+						<p>{project.description.term}</p>
+					</div>
+					<p className="px-3 text-3xl truncate w-full">{project.description.paragraph}</p>
 				</div>
-			</div>
+			</article>
 		</Link>
 	);
 }
