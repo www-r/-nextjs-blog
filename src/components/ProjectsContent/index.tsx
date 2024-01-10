89b0ea6c3479904f18ff6c723acb5e33f7afc7be
김영은 <jessicak0828@gmail.com>
2024-01-10 13:36:46 +0900

    feat: create Component ProjectsContent
    make functions that filters projects by selected filter

---
+++ src/components/ProjectsContent/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { Project } from '@/types';
+import FilterList from '@components/FilterList';
+import Grid from '@components/Grid';
+import { ALL } from '@/constants';
+interface Props {
+	projectArr: Project[];
+}
+
+export default function ProjectsContent({ projectArr }: Props) {
+	const [projects, setProjects] = useState(projectArr);
+	const [filter, setFilter] = useState<string>(ALL);
+
+	useEffect(() => {
+		if (filter === ALL) {
+			setProjects(projectArr);
+		}
+		if (filter !== ALL) {
+			const filteredArr = projectArr.filter((item) => item.category === filter);
+			setProjects(filteredArr);
+		}
+	}, [filter]);
+
+	return (
+		<>
+			<div className="inner flex-row--between mt-5 mx-3 text-3xl">
+				<div className="ml-[5vw]">{`Projects (${projects.length})`}</div>
+				<FilterList setFilter={setFilter} />
+			</div>
+			<div className="py-20">
+				<Grid projects={projects} />
+			</div>
+		</>
+	);
+}
