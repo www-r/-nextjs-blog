77953137fea6674178ae3cc7fa09430eeeb47051
김영은 <jessicak0828@gmail.com>
2024-01-10 13:12:52 +0900

    feat: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,25 @@
+export type Database = 'main' | 'projects';
+// export type Toggle = 'image' | 'profile' | 'skill' | 'education' | 'contact';
 
+export type Profile = {
+	key: string;
+	value: string;
+};
+export type Skill = {
+	name: string;
+	description: string;
+	image: string;
+};
+export type Education = {
+	name: string;
+	when: string;
+	finished: boolean;
+};
 
 export type Project = {
 	title: string;
-	id: string;
+	id: string; //childPageId
+	link: string;
 	image: string;
 	description: {
 		people: string;
@@ -13,4 +30,7 @@ export type Project = {
 	category: 'Front-End' | 'Back-End' | 'Full-Stack' | 'All';
 };
 
-
+export type Contact = {
+	key: string;
+	value: string[];
+};
