3ce8e4763037399316b6cb9751fcef0afa58acd1
김영은 <jessicak0828@gmail.com>
2024-01-10 13:27:48 +0900

    feat: create Component Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,25 @@
-import { PropsWithChildren } from 'react';
+import Link from 'next/link';
+import Icon from '@components/Icon';
 
-interface Props extends PropsWithChildren {}
+import Github from '@/assets/github-mark-white.png';
+import Tistory from '@/assets/logomark.png';
 
-export default function Footer({ children }: Props) {
-	return <footer>{children}</footer>;
+export default function Footer() {
+	return (
+		<footer className="inner bg-brown py-8 w-full text-white text-[2.5rem] flex-cols justify-center items-center">
+			<ul className="inner flex justify-center my-auto">
+				<Link href="https://github.com/www-r">
+					<li className=" w-[6rem] center">
+						<Icon src={Github} alt="github" size={50} />
+					</li>
+				</Link>
+				<Link href="https://woorii-kye.tistory.com">
+					<li>
+						<Icon src={Tistory} alt="tistory" size={64} />
+					</li>
+				</Link>
+			</ul>
+			<p>© 2023. Kim Young En. All rights reserved.</p>
+		</footer>
+	);
 }
