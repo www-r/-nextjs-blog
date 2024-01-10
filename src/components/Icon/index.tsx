ba8dc7042406350d44354c25d62f3a238b4640ae
김영은 <jessicak0828@gmail.com>
2024-01-10 13:34:49 +0900

    feat: create Component Icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -7,5 +7,9 @@ interface Props {
 }
 
 export default function Icon({ src, size = 30, alt }: Props) {
-	return <Image src={src} alt={alt} height={size} width={size} style={{ objectFit: 'contain' }} />;
+	return (
+		<div className="flex items-center my-auto">
+			<Image src={src} alt={alt} height={size} width={size} style={{ objectFit: 'contain' }} />
+		</div>
+	);
 }
