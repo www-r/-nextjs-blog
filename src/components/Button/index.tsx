09a28279de62430733479891304fa74e0ee5311e
김영은 <jessicak0828@gmail.com>
2024-01-19 23:54:37 +0900

    feat: modify Component Button
    - add  ComponentProps at Props Interface
    - add { ...props }

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,36 @@
-import { PropsWithChildren } from 'react';
+'use client';
+import Link from 'next/link';
+import './styles.css';
+import { ComponentProps} from 'react';
 
-interface Props extends PropsWithChildren {
-	className?: string;
+interface Props extends Omit<ComponentProps<'button'>, 'type'> {
+	message?: {
+		en: string;
+		ko: string;
+	};
+	href?: string;
+	type?: 'contact' | 'resume' | 'letter';
 }
 
-export default function Button({ children, className = '' }: Props) {
-	return <button type='button' className={className}>{children}</button>;
+export default function Button({ message, href, type, ...props }: Props) {
+	return (
+		<>
+			{props.onClick ? (
+				<button className={props.className} onClick={props.onClick}>
+					{props.children}
+				</button>
+			) : (
+				<Link href={href}>
+					<button
+						type="button"
+						className={`button ${type} min-w-[18rem] w-[24vw] mb-[1.6rem] py-3 border border-solid rounded-none`}
+					>
+						<p className="text-lg font-header">{message.en}</p>
+						<p className="text-xs  ">{message.ko}</p>
+						{props.children}
+					</button>
+				</Link>
+			)}
+		</>
+	);
 }
