dde9b8fd9c77cd2d54e7e45ba6f23b5082f5d1bb
김영은 <jessicak0828@gmail.com>
2024-01-19 23:48:19 +0900

    feat: add icon assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as Tomato } from './tomato.svg';
+export { default as Books } from './books.svg';
+export { default as Girl } from './girl-medium-light-skin-tone.svg';
+export { default as Tiger } from './tiger-face.svg';
+export { default as Graduation } from './graduation-cap.svg';
--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const MY_NAME = 'Kim Young En';
 //About
 // const ABOUT_CATEGORY = ['Profile','Skills','Education']
 export const PROFILE_TEXT = {
-	main: [`Hi, I'm YoungEn`, 'Front-End Developer'],
+	main: '안녕하세요\n프론트엔드 개발자\n김영은입니다.',
 	about: [
 		`'우리'가 중요한 개발자, 김영은입니다. 협업, 소통, 성장을 중요하게 생각합니다.`,
 		'원활한 협업을 위해 명시적인 코드 작성 및 안정성을 높이려고 항상 노력합니다.',
@@ -22,11 +22,25 @@ export const PATH_NAME = {
 	home: 'home',
 	about: 'about',
 	projects: 'projects',
-	contact: 'contact',
+	guestbook: 'guestbook',
 };
 
 export const BUTTON = {
-	contact: 'Contact me',
-	resume: 'check Resume',
-	letter: 'check Cover Letter',
+	contact: {
+		en: 'Contact',
+		ko: '연락하기',
+	},
+	resume: { en: 'Resume', ko: '이력서보기' },
+	letter: { en: 'Cover Letter', ko: '자기소개서' },
+};
+export const COLOR = {
+	white: '#fff',
+	brown: '#1f1e1d',
+	blue: '#e5f0f3',
+	ivory: 'rgb(251,245,222)',
+	green: '#d5fcdf',
+	mint: '#14e97b',
+	orange: '#ed7c48',
+	pink: '#fed5e7',
+	purple: '#65449e',
 };
