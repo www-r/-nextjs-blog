59c9c25f7ecb69836e91ccac29922c11e45e0b5d
김영은 <jessicak0828@gmail.com>
2024-01-19 23:52:03 +0900

    feat: create functions for api request to insert data, login

---
+++ src/service/supabase.ts
@@ -0,0 +1,69 @@
+import { supabase } from '@/database/supabase';
+
+const DB_TABLE_NAME = 'guestbook';
+
+export async function getDatabaseData(row = '') {
+	let { data: guestbook, error } = await supabase.from(DB_TABLE_NAME).select(row);
+	if (error) {
+		console.error(error);
+	} else {
+		console.log(guestbook);
+		return guestbook;
+	}
+}
+// INSERT A ROW
+export async function insertRow(input) {
+	const { data, error } = await supabase.from(DB_TABLE_NAME).insert(input);
+	console.log('insertRow:', data);
+	if (error) {
+		console.error(error);
+	}
+}
+// INSERT MANY ROWS
+export async function insertManyRows(dataArr) {
+	const { data, error } = await supabase.from(DB_TABLE_NAME).insert(dataArr).select();
+	if (error) {
+		console.error(error);
+	}
+}
+// UPSERT MATCHING ROWS
+export async function upsertMatchingRows() {
+	const { data, error } = await supabase.from(DB_TABLE_NAME).upsert({ some_column: 'someValue' }).select();
+	if (error) {
+		console.error(error);
+	} else {
+		console.log('upsertMatchingRow:', data);
+		return data;
+	}
+}
+
+export async function signInOAuthUser() {
+	// Register this immediately after calling createClient!
+	// Because signInWithOAuth causes a redirect, you need to fetch the
+	// provider tokens from the callback.
+	supabase.auth.onAuthStateChange((event, session) => {
+		if (session && session.provider_token) {
+			window.localStorage.setItem('oauth_provider_token', session.provider_token);
+		}
+
+		if (session && session.provider_refresh_token) {
+			window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token);
+		}
+
+		if (event === 'SIGNED_OUT') {
+			window.localStorage.removeItem('oauth_provider_token');
+			window.localStorage.removeItem('oauth_provider_refresh_token');
+		}
+	});
+	// Call this on your Sign in with GitHub button to initiate OAuth
+	// with GitHub with the requested elevated scopes.
+	await supabase.auth.signInWithOAuth({
+		provider: 'google',
+	});
+}
+export async function signOutOAuthUser() {
+	const { error } = await supabase.auth.signOut();
+	if (error) {
+		console.error(error);
+	}
+}
