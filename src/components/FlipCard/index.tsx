f14babab28fbc25a10f828c663e4dcb6a5da4323
김영은 <jessicak0828@gmail.com>
2024-01-22 21:49:23 +0900

    feat: create Component FlipCard

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -1,28 +1,27 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import './styles.css';
 export default function FlipCard({ children, img }) {
-	const [isIconFront, setIsIconFront] = useState(true);
 	function clickHandler(e) {
-		console.log('clicked@');
-		if (e.target.classList.contains('isFront')) {
-			e.target.classList.remove('isFront');
-		} else {
-			e.target.classList.add('isFront');
-		}
-		setIsIconFront(!isIconFront);
+		e.target.closest('.container').classList.contains('rotate')
+			? e.target.closest('.container').classList.remove('rotate')
+			: e.target.classList.add('rotate');
+		console.log(e.target.closest('.container'));
 	}
 	return (
-		<div className="container">
-			{/* //front */}
-			<div className={`flip-card front ${isIconFront ? 'isFront' : ''}`} onClick={(e) => clickHandler(e)}>
-				<Image src={img} alt="" className="front-image" width={64} height={64} />
-			</div>
-
-			{/* //back */}
-			<div className={`flip-card back ${isIconFront ? '' : 'isFront'}}`} onClick={(e) => clickHandler(e)}>
-				<div>{children}</div>
+		<div className="container" onClick={(e) => clickHandler(e)}>
+			<div className="card-wrapper">
+				{/* //front */}
+				<div className={`flip-card front`}>
+					
+						<Image src={img} alt="" className="front-image" width={64} height={64} />
+			
+				</div>
+				{/* //back */}
+				<div className={`flip-card back`}>
+					<div>{children}</div>
+				</div>
 			</div>
 		</div>
 	);
