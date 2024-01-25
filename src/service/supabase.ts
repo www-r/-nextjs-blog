8424ec696f482a0f6514f100eab0f663ef24ee1a
김영은 <jessicak0828@gmail.com>
2024-01-26 07:43:49 +0900

    feat: add subscription & change redirectTo address to '/guestbook'

--- src/service/supabase.ts
+++ src/service/supabase.ts
@@ -1,22 +1,27 @@
 import { supabase } from '@/database/supabase';
-
 const DB_TABLE_NAME = 'guestbook';
 
-export async function getDatabaseData(row = '') {
-	let { data: guestbook, error } = await supabase.from(DB_TABLE_NAME).select(row);
+export async function getDatabaseData() {
+	let { data: guestbook, error } = await supabase.from(DB_TABLE_NAME).select('*', { count: 'estimated' }).range(0, 10);
 	if (error) {
 		console.error(error);
 	} else {
-		// console.log(guestbook);
+		console.log(guestbook);
 		return guestbook;
 	}
 }
 // INSERT A ROW
 export async function insertRow(input) {
-	const { data, error } = await supabase.from(DB_TABLE_NAME).insert(input);
-	// console.log('insertRow:', data);
-	if (error) {
-		console.error(error);
+	if (input) {
+		try {
+			const { data, error } = await supabase.from(DB_TABLE_NAME).insert([input]).select();
+			console.log('insertRow:', data);
+			alert('작성되었습니다.');
+		} catch (error) {
+			console.error(error);
+		}
+	} else {
+		console.log(input);
 	}
 }
 // INSERT MANY ROWS
@@ -36,39 +41,51 @@ export async function upsertMatchingRows() {
 		return data;
 	}
 }
+// DELETE ROW
+export async function deleteSelectedRow(id: string) {
+	const { error } = await supabase.from(DB_TABLE_NAME).delete().eq('id', id);
+}
 
 export async function signInOAuthUser() {
 	const { data, error } = await supabase.auth.signInWithOAuth({
 		provider: 'google',
 		options: {
-			redirectTo: '/guestbook',
+			redirectTo: 'http://localhost:3000/guestbook',
 			queryParams: {
 				access_type: 'offline',
 				prompt: 'consent',
 			},
 		},
 	});
-
-	if (error) {
-		console.error(error);
-	}
-	console.log('data', data);
-	// window.localStorage.setItem('provider_refresh_token', data);
 }
+export function setSession(session) {
+	if (session && session.provider_token) {
+		window.localStorage.setItem('oauth_provider_token', session.provider_token);
+		console.log('set');
+	}
+
+	if (session && session.provider_refresh_token) {
+		window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token);
+		console.log('set_refresh');
+	}
+}
+
 export async function signOutOAuthUser() {
 	const { error } = await supabase.auth.signOut();
+	alert('로그아웃되었습니다.');
 	if (error) {
 		console.error(error);
 	}
-	alert('로그아웃되었습니다.');
 }
 
 export async function retrieveUser() {
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
-	console.log(user);
+	console.log('user', user);
+	return user;
 }
+
 export async function retrieveSession() {
 	const { data, error } = await supabase.auth.getSession();
 	if (error) {
@@ -82,4 +99,39 @@ export async function retrieveNewSession() {
 	if (error) {
 		console.error(error);
 	}
+}
+
+export function userStateSubscription() {
+	return supabase.auth.onAuthStateChange(async (event, session) => {
+		if (event === 'INITIAL_SESSION') {
+			// handle initial session
+			if (session && session.provider_token) {
+				window.localStorage.setItem('oauth_provider_token', session.provider_token);
+			}
+			if (session && session.provider_refresh_token) {
+				window.localStorage.setItem('oauth_provider_refresh_token', session.provider_refresh_token);
+			}
+			if (session && session.user) {
+				const userInfo = JSON.stringify({ aud: session.user.aud, email: session.user.email, id: session.user.id });
+				window.localStorage.setItem('user', userInfo);
+			}
+		}
+		if (event === 'SIGNED_IN') {
+			// handle sign in event
+		}
+		if (event === 'SIGNED_OUT') {
+			// handle sign out event
+			window.localStorage.removeItem('user');
+			await signOutOAuthUser();
+		}
+		if (event === 'PASSWORD_RECOVERY') {
+			// handle password recovery event
+		}
+		if (event === 'TOKEN_REFRESHED') {
+			// handle token refreshed event
+		}
+		if (event === 'USER_UPDATED') {
+			// handle user updated event
+		}
+	});
 }
