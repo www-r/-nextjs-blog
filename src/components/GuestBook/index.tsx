c6cad6b173d6c3950f406816d0f69badfff1216f
김영은 <jessicak0828@gmail.com>
2024-01-26 07:40:40 +0900

    feat: render commentlists when mounted

--- src/components/GuestBook/index.tsx
+++ src/components/GuestBook/index.tsx
@@ -1,32 +1,39 @@
 'use client';
-import Form from '@components/Form';
-import CommentListItem from '@components/CommentListItem';
-import { getDatabaseData } from '@/service/supabase';
-import Icon from '../Icon';
-import { Comment } from '@/assets';
 import { useEffect, useState } from 'react';
+import Form from '@components/Form';
+import CommentsContainer from '@components/CommentsContainer';
+import { getDatabaseData, signOutOAuthUser, userStateSubscription } from '@/service/supabase';
+import { CommentData } from '@/types';
 
 export default function GuestBook() {
-	// const dataArr = await getDatabaseData();
-	const [dataArr, setDataArr] = useState([]);
+	const [isAuthorized, setIsAuthorized] = useState(false);
+	const [dataArr, setDataArr] = useState<CommentData[]>([]);
+	function checkIfAuthorized() {
+		if (localStorage.getItem('user')) {
+			setIsAuthorized(true);
+		}
+	}
 	useEffect(() => {
-		getDatabaseData().then((data) => setDataArr(data));
+		getDatabaseData().then((data) => {
+			const sorted = data.sort((a, b) => b - a);
+			setDataArr(sorted);
+		});
+	}, []);
+
+	useEffect(() => {
+		checkIfAuthorized();
+		userStateSubscription();
 	}, []);
 	return (
 		<div>
 			<div className=" inner">
 				{/* <h2 className="text-2xl mt-10 border-b border-solid">Guest Book</h2> */}
-				<Form />
+				{isAuthorized && <button onClick={async () => await signOutOAuthUser()}>로그아웃하기</button>}
+				<Form isAuthorized={isAuthorized} />
 				<div className=" mt-5  h-[50vh] p-3 overflow-auto bg-white/80 rounded-md">
-					<ul>{dataArr.length && dataArr?.map((data) => <CommentListItem data={data} key={data.id} />)}</ul>
+					<CommentsContainer dataArr={dataArr} />
 				</div>
 			</div>
-			<Icon
-				src={Comment}
-				alt=""
-				size={50}
-				className="fixed bottom-10 right-20 z-10 bg-white rounded-full p-5 hover:cursor-pointer"
-			/>
 		</div>
 	);
 }
