4f5527ead5581650119f22842e3797c7a1b39393
김영은 <jessicak0828@gmail.com>
2024-01-31 23:26:09 +0900

    feat: write logics to get README.md file from repository and convert them into HTML to render on screen

---
+++ src/service/github.ts
@@ -0,0 +1,29 @@
+import { octokit } from '@/service';
+const GITHUB_OWNER = 'www-r';
+
+export async function getRepoReadMe(owner = GITHUB_OWNER as string | undefined, repo: string) {
+	const response = await octokit.request(`GET /repos/${owner}/${repo}/readme`, {
+		owner: owner,
+		repo: repo,
+		headers: {
+			'X-GitHub-Api-Version': '2022-11-28',
+			accept: 'application/vnd.github.raw+json',
+		},
+	});
+	// console.log('ddd', response);
+	return response.data;
+}
+
+export async function getMarkdownRaw(markdownText: string) {
+	const res = await octokit.request('POST /markdown', {
+		text: markdownText,
+		mode: 'markdown',
+		// data: '# Markdownmasrkdksjflksdlsjf',
+		headers: {
+			'X-GitHub-Api-Version': '2022-11-28',
+			'Content-Type': 'text/x-markdown',
+		},
+	});
+	console.log('ddd', res.data);
+	return res.data;
+}
