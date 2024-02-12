733673296e150bf99977b5fa7040abf3ee04e34a
김영은 <jessicak0828@gmail.com>
2024-02-12 15:47:56 +0900

    feat: edit modal style

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -8,13 +8,26 @@ export default function Modal({}: Props) {
 	const { message } = useContext(ModalMessageContext);
 
 	return (
-		<dialog style={state ? { display: 'block' } : { display: 'none' }}>
-			<p>{message}</p>
-			<form action="">
-				<button value="close" onClick={() => setState(false)}>
-					창 닫기
-				</button>
-			</form>
-		</dialog>
+		<>
+			<div
+				style={state ? { display: 'block' } : { display: 'none' }}
+				className="bg-[rgba(0,0,0,0.7)] inset-0 z-[9] fixed"
+			></div>
+			<dialog
+				style={state ? { display: 'block' } : { display: 'none' }}
+				className="fixed z-10 top-1/3 left-0 py-10 px-20 rounded-md "
+			>
+				<p className="text-md">{message}</p>
+				<form action="">
+					<button
+						value="close"
+						onClick={() => setState(false)}
+						className="w-full border border-solid rounded-md my-3 mx-auto"
+					>
+						창 닫기
+					</button>
+				</form>
+			</dialog>
+		</>
 	);
 }
