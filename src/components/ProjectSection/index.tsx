7c49b5e0302800ec8342f907379f65863060308c
김영은 <jessicak0828@gmail.com>
2024-02-24 18:59:25 +0900

    fix: 프로젝트 상세페이지 css reset 효과 없애기

--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -2,12 +2,15 @@
 import './styles.css';
 import { useState, useEffect } from 'react';
 import { getRepoReadMe, getMarkdownRaw } from '@/service/github';
+
+const GITHUB_ID = 'www-r';
+
 export default async function ProjectSection({ github }) {
 	const [data, setData] = useState<string>('');
 	useEffect(() => {
-		getRepoReadMe('www-r', `${github}`)
+		getRepoReadMe(GITHUB_ID, `${github}`)
 			.then((res) => getMarkdownRaw(res))
 			.then((data) => setData(data));
-	}, []);
+	}, [github]);
 	return <div className="project-container" dangerouslySetInnerHTML={{ __html: `${data}` }}></div>;
 }
